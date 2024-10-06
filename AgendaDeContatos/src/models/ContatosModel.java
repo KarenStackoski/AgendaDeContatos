15dd52d7fd34e37f897e75a801a7efcbce89fb90
Karen Bialescki Stackoski <stackoskikaren@gmail.com>
2024-10-06 15:17:47 -0300

    model

---
+++ AgendaDeContatos/src/models/ContatosModel.java
@@ -0,0 +1,43 @@
+package models;
+
+public class ContatosModel {
+	public String nome;
+	public String telefone;
+	public String endereco;
+	
+	public ContatosModel() {
+		
+	}
+	
+	public ContatosModel(String nome, String telefone, String endereco) {
+		this.nome = nome;
+		this.telefone = telefone;
+		this.endereco = endereco;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
+	public String getEndereco() {
+		return endereco;
+	}
+
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+	
+	
+}
